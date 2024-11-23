ac95c9eadea86fd8f977ea52f8c0e970842802aa
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-11-22 21:59:21 -0500

    Remove a unneeded root div element and a p element enclosing the children

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -10,36 +10,34 @@ function SectionInfo({
   direction,
 }) {
   return (
-    <div>
-      <article>
-        <header className="pb-4">
-          <ConditionalAnimation
-            animate={animate}
-            cascade={cascade}
-            direction={direction}
-          >
-            <h2 className="font-headings text-sm font-bold text-orange-600">
-              {header1}
-            </h2>
-            <h3 className={`font-headings text-3xl font-bold ${textColor}`}>
-              {header2}
-            </h3>
-          </ConditionalAnimation>
-        </header>
-
-        <div
-          className={`pb-8 font-body text-lg ${textColor} md:max-w-sm lg:max-w-md`}
+    <article>
+      <header className="pb-4">
+        <ConditionalAnimation
+          animate={animate}
+          cascade={cascade}
+          direction={direction}
         >
-          <ConditionalAnimation
-            animate={animate}
-            cascade={cascade}
-            direction={direction}
-          >
-            <p>{children}</p>
-          </ConditionalAnimation>
-        </div>
-      </article>
-    </div>
+          <h2 className="font-headings text-sm font-bold text-orange-600">
+            {header1}
+          </h2>
+          <h3 className={`font-headings text-3xl font-bold ${textColor}`}>
+            {header2}
+          </h3>
+        </ConditionalAnimation>
+      </header>
+
+      <div
+        className={`pb-8 font-body text-lg ${textColor} md:max-w-sm lg:max-w-md`}
+      >
+        <ConditionalAnimation
+          animate={animate}
+          cascade={cascade}
+          direction={direction}
+        >
+          <div> {children} </div>
+        </ConditionalAnimation>
+      </div>
+    </article>
   );
 }
 

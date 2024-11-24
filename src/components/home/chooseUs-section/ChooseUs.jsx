ca4a6d1d166770936361735eb3f41d093c7ad75b
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-11-23 21:16:40 -0500

    Add ChooseUs section with sticky cards

---
+++ src/components/home/chooseUs-section/ChooseUs.jsx
@@ -0,0 +1,118 @@
+function ChooseUs() {
+  return (
+    <div
+      style={{ minHeight: "100vh" }}
+      className="relative flex flex-col bg-gray-200 px-4 pb-12 pt-24 font-sans text-gray-700 sm:px-6 lg:px-8"
+    >
+      <div className="flex-1">
+        <div
+          style={{ top: "calc(1rem * 1)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #1 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 2)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #2 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 3)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #3 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 4)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #4 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 5)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #5 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 6)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #6 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+        <div
+          style={{ top: "calc(1rem * 7)" }}
+          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+        >
+          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
+            <span className="block text-sm text-blue-700">Project #7 </span>
+            <span className="block">Title of the Project</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
+            iure eligendi quos eos et autem ratione exercitationem earum laborum
+            ad a sequi!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChooseUs;

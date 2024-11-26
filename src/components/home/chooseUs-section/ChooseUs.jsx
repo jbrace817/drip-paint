108266c89c34a1806efd5406951592d8657893bb
James Brace <48393754+jbrace817@users.noreply.github.com>
2024-11-25 23:52:35 -0500

    style: Update first card in ChooseUs component

--- src/components/home/chooseUs-section/ChooseUs.jsx
+++ src/components/home/chooseUs-section/ChooseUs.jsx
@@ -1,19 +1,27 @@
+import { IoColorFill, IoShieldHalf } from "react-icons/io5";
+
 function ChooseUs() {
   return (
     <div
       style={{ minHeight: "100vh" }}
-      className="relative flex flex-col bg-gray-200 px-4 pb-12 pt-24 font-sans text-gray-700 sm:px-6 lg:px-8"
+      className="relative flex flex-col bg-white px-4 py-16 font-sans sm:px-6 lg:px-8"
     >
-      <div className="flex-1">
+      <div className="flex-1 space-y-6">
         <div
           style={{ top: "calc(1rem * 1)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+          className="sticky mx-auto w-full max-w-md space-y-6 rounded-xl border bg-white px-8 py-12 shadow-md"
         >
-          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
-            <span className="block text-sm text-blue-700">Project #1 </span>
-            <span className="block">Title of the Project</span>
+          <div className="w-fit rounded-xl bg-orange-100 p-3">
+            <IoShieldHalf color="#ea580c" size={"32px"} />
+          </div>
+          <h2 className="space-y-1 text-2xl font-bold leading-none">
+            {/* <span className="block text-sm text-blue-700">Project #1 </span> */}
+
+            <span className="block font-headings text-3xl">
+              Licensed and Insured
+            </span>
           </h2>
-          <p>
+          <p className="font-body text-lg text-gray-700">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
             perspiciatis blanditiis accusamus commodi consectetur id tempora rem
             iure eligendi quos eos et autem ratione exercitationem earum laborum
@@ -22,7 +30,7 @@ function ChooseUs() {
         </div>
         <div
           style={{ top: "calc(1rem * 2)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+          className="sticky mx-auto w-full max-w-md space-y-4 rounded-xl border bg-white px-8 py-12 shadow-lg"
         >
           <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
             <span className="block text-sm text-blue-700">Project #2 </span>
@@ -37,7 +45,7 @@ function ChooseUs() {
         </div>
         <div
           style={{ top: "calc(1rem * 3)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+          className="sticky mx-auto w-full max-w-md space-y-4 rounded-xl border bg-white px-8 py-12 shadow-lg"
         >
           <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
             <span className="block text-sm text-blue-700">Project #3 </span>
@@ -52,55 +60,10 @@ function ChooseUs() {
         </div>
         <div
           style={{ top: "calc(1rem * 4)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
+          className="sticky mx-auto w-full max-w-md space-y-4 rounded-xl border bg-white px-8 py-12 shadow-lg"
         >
           <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
             <span className="block text-sm text-blue-700">Project #4 </span>
-            <span className="block">Title of the Project</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
-            iure eligendi quos eos et autem ratione exercitationem earum laborum
-            ad a sequi!
-          </p>
-        </div>
-        <div
-          style={{ top: "calc(1rem * 5)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
-        >
-          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
-            <span className="block text-sm text-blue-700">Project #5 </span>
-            <span className="block">Title of the Project</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
-            iure eligendi quos eos et autem ratione exercitationem earum laborum
-            ad a sequi!
-          </p>
-        </div>
-        <div
-          style={{ top: "calc(1rem * 6)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
-        >
-          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
-            <span className="block text-sm text-blue-700">Project #6 </span>
-            <span className="block">Title of the Project</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-            perspiciatis blanditiis accusamus commodi consectetur id tempora rem
-            iure eligendi quos eos et autem ratione exercitationem earum laborum
-            ad a sequi!
-          </p>
-        </div>
-        <div
-          style={{ top: "calc(1rem * 7)" }}
-          className="sticky mx-auto w-full max-w-xl space-y-4 rounded-lg border bg-white px-8 py-12 shadow-lg"
-        >
-          <h2 className="space-y-1 text-2xl font-bold leading-none text-gray-900">
-            <span className="block text-sm text-blue-700">Project #7 </span>
             <span className="block">Title of the Project</span>
           </h2>
           <p>

243dae95970b28b27440f2652ef3684da6609889
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-12-07 14:47:46 -0500

    Cleanup unnecessary code on the HomeGallery file

--- src/components/home/gallery-section/HomeGallery.jsx
+++ src/components/home/gallery-section/HomeGallery.jsx
@@ -77,64 +77,6 @@ const photos = [
   },
 ];
 
-// function HomeGallery() {
-//   const [category, setCategory] = useState("exterior");
-//   const [loading, setLoading] = useState(false);
-
-//   function handleClick(newCategory) {
-//     setCategory(newCategory);
-//   }
-
-//   return (
-//     <section>
-//       <div className="mx-auto flex max-w-[80rem] flex-wrap items-center justify-center py-4 md:py-8">
-//         <button
-//           type="button"
-//           className={`mb-3 me-3 rounded-full border border-orange-950 px-5 py-2.5 text-center font-body text-base font-medium text-orange-950 transition ${category === "exterior" ? "bg-orange-100 outline-none ring-4 ring-orange-300" : "bg-white"}`}
-//           onClick={() => handleClick("exterior")}
-//         >
-//           Exterior
-//         </button>
-//         <button
-//           type="button"
-//           className={`mb-3 me-3 rounded-full border border-orange-950 px-5 py-2.5 text-center font-body text-base font-medium text-orange-950 transition ${category === "interior" ? "bg-orange-100 outline-none ring-4 ring-orange-300" : "bg-white"}`}
-//           onClick={() => handleClick("interior")}
-//         >
-//           Interior
-//         </button>
-//         <button
-//           type="button"
-//           className={`mb-3 me-3 rounded-full border border-orange-950 px-5 py-2.5 text-center font-body text-base font-medium text-orange-950 transition ${category === "custom" ? "bg-orange-100 outline-none ring-4 ring-orange-300" : "bg-white"}`}
-//           onClick={() => handleClick("custom")}
-//         >
-//           Custom Spaces
-//         </button>
-//       </div>
-//       <div className="relative mx-auto mb-16 max-w-[80rem]">
-//         {["exterior", "interior", "custom"].map((cat) => (
-//           <div
-//             key={cat}
-//             className={`grid grid-cols-2 gap-4 px-4 md:grid-cols-3 ${
-//               category === cat ? "opacity-100" : "absolute opacity-0"
-//             }`}
-//           >
-//             {photos
-//               .filter((photo) => photo.category === cat)
-//               .map((photo) => (
-//                 <img
-//                   className="h-auto max-w-full rounded-lg"
-//                   src={photo.src}
-//                   alt={photo.alt}
-//                   key={photo.id}
-//                 />
-//               ))}
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-
 function HomeGallery() {
   const [category, setCategory] = useState("exterior");
 

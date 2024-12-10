21b11f21ba3e06c850d84d79b606b46e1442c12a
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-12-10 12:42:13 -0500

    Replace AboutUs jpg files with webp and place them in the public directory

--- src/components/home/about-section/AboutUs.jsx
+++ src/components/home/about-section/AboutUs.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import front from "../../../app/assets/home/about/aboutUsFront_m.jpeg";
-import back from "../../../app/assets/home/about/aboutUsBack_m.jpeg";
+import front from "../../../../public/assets/home/about/aboutUsFront.webp";
+import back from "../../../../public/assets/home/about/aboutUsBack.webp";
 
 import SectionInfo from "@/components/SectionInfo";
 import ConditionalAnimation from "@/utils/ConditionalAnimation";
 function AboutUs() {
   return (
-    <section className="space-y-6 overflow-hidden bg-gradient-to-b from-orange-200 from-5% to-white to-70% px-4 py-16">
+    <section className="space-y-6 overflow-hidden bg-gradient-to-b from-orange-200 from-5% to-white to-70% px-4 py-16 md:pb-16 md:pt-24">
       <div className="m-auto max-w-[80rem] gap-6 space-y-6 md:flex md:justify-between md:space-y-0">
         <SectionInfo
           header1={"WHO WE ARE"}
@@ -31,7 +31,13 @@ function AboutUs() {
             cascade={true}
             className="relative z-10 col-start-1 col-end-9 row-start-1 row-end-4 lg:-mt-8"
           >
-            <Image src={back} alt="Painters at work"></Image>
+            <Image
+              src={back}
+              alt="Painters at work"
+              width={1281} // Intended width for the largest image
+              height={854} // Intended height for the largest image
+              sizes="(min-width: 2000px) 22.7vw, (min-width: 1400px) 427px, (min-width: 780px) calc(29.67vw + 18px), calc(66.52vw - 21px)"
+            ></Image>
           </ConditionalAnimation>
           <ConditionalAnimation
             animate={true}
@@ -39,7 +45,13 @@ function AboutUs() {
             cascade={true}
             className="relative z-20 col-start-5 col-end-13 row-start-3 row-end-6"
           >
-            <Image src={front} alt="Painters looking at blueprint"></Image>
+            <Image
+              src={front}
+              alt="Painters looking at blueprint"
+              width={1281} // Intended width for the largest image
+              height={854} // Intended height for the largest image
+              sizes="(min-width: 2000px) 22.7vw, (min-width: 1400px) 427px, (min-width: 780px) calc(29.67vw + 18px), calc(66.52vw - 21px)"
+            ></Image>
           </ConditionalAnimation>
         </div>
       </div>

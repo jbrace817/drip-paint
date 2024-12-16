295fe2b88dd11699dfb9cacf12373d8a0f869237
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-12-16 14:09:19 -0500

    security: Add path to folder for cloudinary media library

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
+        pathname: "/dsjx8ner3/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
         pathname: "**",
       },
     ],

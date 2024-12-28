e29c9ad7384be665e15218858d9f6e7ea4ee511f
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-12-27 23:21:04 -0500

    Remove local images and retrieve Images from cloudinary cdn
    Updated Alt property for each image

--- src/components/home/services-section/serviceList.js
+++ src/components/home/services-section/serviceList.js
@@ -1,31 +1,33 @@
-import exterior from "../../../../public/assets/home/dripServices/freepik__adjust__29652.png";
-import staircase from "../../../../public/assets/home/dripServices/freepik__adjust__29652.png";
-import interior from "../../../../public/assets/home/dripServices/freepik__adjust__29652.png";
-
 const serviceList = [
   {
     id: "01",
     name: "Exterior",
     description:
       "A quality paint job protects your home’s exterior and boosts curb appeal. At Drip Painting, we ensure a durable, beautiful finish with expert techniques and the best products.",
-    imageSrc: exterior,
-    imageAlt: "Printed photo of bag being tossed into the sky on top of grass.",
+    imageSrc:
+      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1735358150/freepik__adjust__29652_ozcjoj.webp",
+    imageAlt:
+      "Exterior of a house painted light green with decorative windows and elegant accents.",
   },
   {
     id: "02",
     name: "Interior",
     description:
       "Refresh your home effortlessly with Drip Painting. We deliver flawless results while protecting your space and ensuring a smooth, stress-free experience.",
-    imageSrc: staircase,
-    imageAlt: "Beautiful re-painted staircase",
+    imageSrc:
+      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1735358150/freepik__adjust__2409_ge9ick.webp",
+    imageAlt:
+      "Interior of a residential room featuring a window, maroon-colored wall, sofa, and shag carpet flooring.",
   },
   {
     id: "03",
     name: "Custom Finishes",
     description:
       "Custom finishes add character and elegance to your space. At Drip Painting, we create unique textures and details, crafting a personalized look that reflects your style.",
-    imageSrc: interior,
-    imageAlt: "Black canvas body with chrome zipper and key ring.",
+    imageSrc:
+      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1735358151/freepik__adjust__85876_cpjqxu.webp",
+    imageAlt:
+      "Modern white kitchen cabinets with gray countertops in a clean, minimalist design.",
   },
 ];
 

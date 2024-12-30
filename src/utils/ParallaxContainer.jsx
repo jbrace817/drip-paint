378c2fc34b0270a85b9812d37addde49ceecbebb
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-12-30 00:52:23 -0500

    fix: Correct an dynamic import error
    Because jarallax is a client-side library you have to disable server side rendering

--- src/utils/ParallaxContainer.jsx
+++ src/utils/ParallaxContainer.jsx
@@ -1,20 +1,30 @@
 "use client";
+import dynamic from "next/dynamic";
 import { useEffect, useRef } from "react";
-import { jarallax } from "jarallax";
+
+const jarallax = dynamic(() => import("jarallax").then((mod) => mod.jarallax), {
+  ssr: false,
+});
 
 function ParallaxContainer({ image, speed, children }) {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    if (containerRef.current) {
-      jarallax(containerRef.current, {
-        speed: speed || 0.5,
-        imgSrc: image,
-      });
-    }
+    const initJarallax = async () => {
+      if (containerRef.current && jarallax) {
+        const { jarallax } = await import("jarallax");
+        jarallax(containerRef.current, {
+          speed: speed || 0.5,
+          imgSrc: image,
+        });
+      }
+    };
+
+    initJarallax();
 
     return () => {
       if (containerRef.current) {
+        const { jarallax } = require("jarallax");
         jarallax(containerRef.current, "destroy");
       }
     };

d46ac55fe49cd20be9792ec3d9bc3373caeaaa6f
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-01-08 15:02:16 -0500

    Add initial code for new navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,67 +1,242 @@
 "use client";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Logo from "../../app/assets/navbar/dripLogo.svg";
+import Logo from "/public/assets/logo/navbar/dripLogo.svg";
 import Link from "next/link";
-import MainNav from "./MainNav";
-import MobileNav from "./MobileNav";
+import Image from "next/image";
 
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(true);
-  const [isTop, setIsTop] = useState(true); // Track if at the top of the screen
-  const [prevScroll, setPrevScroll] = useState(0);
+import { useState } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/react";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 
-  useEffect(() => {
-    const SCROLL_THRESHOLD = 10; // Adjust based on sensitivity
+const products = [
+  {
+    name: "Analytics",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Engagement",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Security",
+    description: "Your customers’ data will be safe and secure",
+    href: "#",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools",
+    href: "#",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Automations",
+    description: "Build strategic funnels that will convert",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
+const callsToAction = [
+  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
+  { name: "Contact sales", href: "#", icon: PhoneIcon },
+];
+const company = [
+  { name: "About us", href: "#" },
+  { name: "Careers", href: "#" },
+  { name: "Support", href: "#" },
+  { name: "Press", href: "#" },
+  { name: "Blog", href: "#" },
+];
 
-    const handleScroll = () => {
-      const currentScroll = Math.max(0, Math.round(window.scrollY));
-
-      // Check if at the top of the screen
-      setIsTop(currentScroll === 0);
-
-      // Toggle visibility only if the scroll difference exceeds the threshold
-      if (Math.abs(currentScroll - prevScroll) > SCROLL_THRESHOLD) {
-        setIsVisible(currentScroll < prevScroll || currentScroll === 0);
-        setPrevScroll(currentScroll);
-      }
-    };
-
-    // Disables scroll when mobile menu is open
-    document.body.style.overflow = `${isOpen ? "hidden" : "visible"}`;
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScroll, isOpen]);
+export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header
-      className={`fixed left-0 top-0 z-[100] w-full transition duration-300 ${
-        isVisible ? "translate-y-0" : "-translate-y-full"
-      } ${isTop ? "bg-transparent" : "bg-white/60 backdrop-blur-md"}`}
-    >
-      <div className="container m-auto flex h-full max-w-[80rem] items-center justify-between p-2">
-        {/* Logo */}
-        <Link href="/" aria-label="Home">
-          <Image
-            src={Logo}
-            width={0}
-            unoptimized
-            className="w-28"
-            alt="Drip Company Logo"
-          />
-        </Link>
-        <nav aria-label="Main Navigation">
-          {/* Desktop */}
-          <MainNav />
+    <header className="fixed left-0 top-0 z-[100] w-full bg-white/50 backdrop-blur-md">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-[80rem] items-center justify-between px-4 py-5 xl:px-0"
+      >
+        <div className="flex lg:flex-1">
+          {/* Logo */}
+          <Link href="/" aria-label="Home">
+            <span className="sr-only">Your Company</span>
+            <Image
+              src={Logo}
+              width={0}
+              unoptimized
+              className="w-28"
+              alt="Drip Company Logo"
+            />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Features
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Product
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Marketplace
+          </a>
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 focus-visible:outline-none">
+                  Company
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className={`size-5 flex-none text-gray-400 transition duration-300 ease-in-out ${
+                      open ? "rotate-180" : ""
+                    }`}
+                  />
+                </PopoverButton>
 
-          {/* Mobile */}
-          <MobileNav isOpen={isOpen} setIsOpen={setIsOpen} />
-        </nav>
-      </div>
+                <PopoverPanel
+                  transition
+                  className="absolute -left-8 top-full z-10 mt-3 w-40 rounded-xl bg-white p-2 shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+                >
+                  {company.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block rounded-lg px-3 py-2 text-sm/6 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </PopoverPanel>
+              </>
+            )}
+          </Popover>
+        </PopoverGroup>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-[110] w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            {/* Logo */}
+            <Link href="/" aria-label="Home">
+              <span className="sr-only">Your Company</span>
+              <Image
+                src={Logo}
+                width={0}
+                unoptimized
+                className="w-24"
+                alt="Drip Company Logo"
+              />
+            </Link>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Product
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+
+                <Disclosure as="div" className="-mx-3">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    Company
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="size-5 flex-none group-data-[open]:rotate-180"
+                    />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2">
+                    {company.map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
     </header>
   );
 }
-
-export default Navbar;
